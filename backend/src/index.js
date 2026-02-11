c9c1498186f12262792af1dc1e4bffc2e41d0933
Satyam Gagare <satyamgagre4@gmail.com>
2026-02-11 23:58:11 +0530

    update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,6 +16,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // to parse req.body
+
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
--- backend/src/routes/auth.Route.js
+++ backend/src/routes/auth.Route.js
@@ -1,10 +1,13 @@
 import {Router} from "express";
+import {User} from "../models/user.model.js"
 
 const router = Router();
 
 router.post("/callback",  async(req, res) => {
     try {
         const { id, firstName, lastName, imageUrl } = req.body;
+
+        const user = await User.findOne({clerkId: id});
     } catch (error) {
         
     }
